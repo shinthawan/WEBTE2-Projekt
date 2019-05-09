bbea014a10f679a53a31256d9c3c54da565d11d7
Schurdy <46352499+Schurdy@users.noreply.github.com>
2019-05-09 21:03:52 +0200

    File ready for implementing student view

---
+++ studentView.php
@@ -0,0 +1,23 @@
+<?php
+echo "Hello i like you.";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Import výsledkov</h2>
+        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+    </div>
+</body>
+</html>
