5f88a53bc7e18bf9e4b154ebc90a3e6075d61f7d
Schurdy <46352499+Schurdy@users.noreply.github.com>
2019-05-19 13:49:58 +0200

    Add files via upload

--- config.php
+++ config.php
@@ -6,11 +6,16 @@ define('DB_USERNAME', 'admin');
 define('DB_PASSWORD', 'adminoveheslo');
 define('DB_NAME', 'FinalProject');
 
-/* Attempt to connect to MySQL database */
-$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
+/* Attempt to connect to MySQL database */
+
+$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$conn->set_charset("utf8");
 // Check connection
+
 if($mysqli === false){
     die("ERROR: Could not connect. " . $mysqli->connect_error);
 }
+
 ?>
