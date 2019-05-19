a1ac1bf7767dbac1fe2eef3a7d460fddbba1180e
Schurdy <46352499+Schurdy@users.noreply.github.com>
2019-05-19 13:47:23 +0200

    new version of index.php

--- index.php
+++ index.php
@@ -4,17 +4,36 @@ session_start();
 // Include config file
 require_once "uloha1/config.php";
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Login app</title>
-    <meta name="google-site-verification" content="Nm8_jfnV7uC9sELhiGjZhFj4XAWI1ZDAm5G49D_vV70" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
+<body>
+
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">WEBTE 2</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="uloha1/uloha1menu.php">Uloha 1</a></li>           <!-- tu odkazujem na svoje menu v ulohe tam sa potom riesi login atd-->
+            <li><a href="#">Uloha 2</a></li>
+            <li><a href="#">uloha 3</a></li>
+        </ul>
+    </div>
+</nav>
+
 <div class="container">
-    <h1>Vyber si spôsob prihlásenia</h1>
-    <a href="loginStudent.php?" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Login študent</a>
-    <a href="loginAdmin.php?" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Login admin</a>
-    <a href="loginLdap.php?" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Login LDAP</a>
+    <h3>Basic Navbar Example</h3>
+    <p>A navigation bar is a navigation header that is placed at the top of the page.</p>
 </div>
+
+</body>
 </html>
